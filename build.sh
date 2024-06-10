dd470ce91b9e07192dfd97a819ba26ab6cbe6c1d
pravinraghul <pravinraghul@gmail.com>
2024-06-10 19:46:46 +0530

    Completed assignment 6 part 2

--- build.sh
+++ build.sh
@@ -10,6 +10,9 @@ git submodule update
 source poky/oe-init-build-env
 
 CONFLINE="MACHINE = \"qemuarm64\""
+CONF_DL_DIR="DL_DIR = \"/home/pravinraghul/yocto/downloads\""
+CONF_SSTATE_DIR="SSTATE_DIR = \"/home/pravinraghul/yocto/sstate-cache\""
+CONF_TMPDIR="TMPDIR = \"/home/pravinraghul/yocto/tmp\""
 
 cat conf/local.conf | grep "${CONFLINE}" > /dev/null
 local_conf_info=$?
@@ -17,11 +20,13 @@ local_conf_info=$?
 if [ $local_conf_info -ne 0 ];then
 	echo "Append ${CONFLINE} in the local.conf file"
 	echo ${CONFLINE} >> conf/local.conf
+	echo ${CONF_DL_DIR} >> conf/local.conf
+	echo ${CONF_SSTATE_DIR} >> conf/local.conf
+	echo ${CONF_TMPDIR} >> conf/local.conf
 	
 else
 	echo "${CONFLINE} already exists in the local.conf file"
 fi
-
 
 bitbake-layers show-layers | grep "meta-aesd" > /dev/null
 layer_info=$?
